590899ac5e6adc0615ae26c97c93a9ef9220ce2a
sohaib-albyari <salbyari3@gmail.com>
2024-05-09 12:29:37 +0300

    active all function

--- assest/main.js
+++ assest/main.js
@@ -1,12 +1,14 @@
 const frameeVideo = document.querySelector(".frameeVideo");
 const circle = document.querySelector(".circle");
-const btns = document.querySelectorAll(".btns button");
+const btns = document.querySelector(".btns");
+const btn = document.querySelectorAll(".btns button");
 const imageSvg = document.querySelectorAll(".btns button img");
 const btn_play_pause = document.querySelector(".btn_play_pause");
 const video_container = document.querySelector(".video-container");
 const videoElement = video_container.querySelector("#video");
 const startRecordButton = document.getElementById("start");
 const stopRecordButton = document.getElementById("stopRecord");
+const pauseRecordButton = document.getElementById("pauseRecord");
 const playRecordButton = document.getElementById("playRecord");
 const downloadRecordButton = document.getElementById("downloadRecord");
 const recordedVideo = document.createElement("video");
@@ -15,14 +17,27 @@ const userIcon = document.querySelector(".userIcon");
 var mediaRecorder;
 var recordedChunks = [];
 
-btns.forEach((e, i) => {
+const addBtn = `
+<button class="btn" onclick="playRecord()" id="playRecord">
+        <img src="assest/image/icon/Play.svg" /><span class="text"
+          >تشغيل التسجيل</span
+        >
+      </button>
+      <button class="btn" onclick="download()" id="downloadRecord">
+        <img src="assest/image/icon/download.svg" /><span class="text"
+          >تنزيل التسجيل</span
+        >
+      </button>
+`;
+
+btn.forEach((e, i) => {
   e.addEventListener("click", () => {
     switch (e.id) {
       case "start":
         e.style.display = "none";
         btn_play_pause.style.display = "flex";
         break;
-      case "startRecord":
+      case "pauseRecord":
         imageSvg[i].src = "assest/image/icon/Play.svg";
         imageSvg[i].classList.toggle("stop");
         if (!imageSvg[i].classList.contains("stop")) {
@@ -31,6 +46,8 @@ btns.forEach((e, i) => {
         break;
       case "stopRecord":
         imageSvg[i].classList.toggle("stop");
+        btns.innerHTML = "";
+        btns.innerHTML = addBtn;
         break;
       case "camera":
         imageSvg[i].src = "assest/image/icon/S_Camera.svg";
@@ -75,8 +92,9 @@ startRecordButton.addEventListener("click", () => {
   userIcon.style.display = "none";
   videoElement.style.display = "block";
   frameeVideo.style.backgroundImage = "url(assest/image/RFG.svg)";
-  circle.style.backgroundColor = "var(--red)";
-  document.body.style.backgroundImage = "none";
+  circle.style.backgroundColor = "var(--green)";
+  circle.style.display = "block";
+  // document.body.style.backgroundImage = "none";
   // frameeVideo.classList.add("active");
   recordedChunks = [];
   mediaRecorder.start();
@@ -86,35 +104,44 @@ startRecordButton.addEventListener("click", () => {
 
 stopRecordButton.addEventListener("click", () => {
   mediaRecorder.stop();
-  // startRecordButton.disabled = false;
-  // stopRecordButton.disabled = true;
 });
 
-// downloadRecordButton.addEventListener("click", () => {
-//   const blob = new Blob(recordedChunks, { type: "video/mp4" });
-//   const url = URL.createObjectURL(blob);
-//   const a = document.createElement("a");
-//   document.body.appendChild(a);
-//   a.style = "display: none";
-//   a.href = url;
-//   a.download = "recording.mp4";
-//   a.click();
-//   window.URL.revokeObjectURL(url);
-// });
+pauseRecordButton.addEventListener("click", () => {
+  circle.classList.toggle("animation");
+  if (imageSvg[1].classList.contains("stop")) {
+    mediaRecorder.pause();
+    circle.style.backgroundColor = "var(--red)";
+  } else {
+    mediaRecorder.resume();
+    circle.style.backgroundColor = "var(--green)";
+  }
+});
 
-// playRecordButton.addEventListener("click", () => {
-//   console.log(video_container);
-//   const blob = new Blob(recordedChunks, { type: "video/mp4" });
-//   const url = URL.createObjectURL(blob);
-//   video_container.innerHTML = "";
-//   if (blob.size != 0) {
-//     recordedVideo.controls = true;
-//     recordedVideo.autoplay = true;
-//     recordedVideo.src = url;
-//     recordedVideo.id = "video";
-//     recordedVideo.style.display = "block";
-//     video_container.appendChild(recordedVideo);
-//   }
-// });
+function download() {
+  const blob = new Blob(recordedChunks, { type: "video/mp4" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = "recording.mp4";
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+
+function playRecord() {
+  console.log(video_container);
+  const blob = new Blob(recordedChunks, { type: "video/mp4" });
+  const url = URL.createObjectURL(blob);
+  video_container.innerHTML = "";
+  if (blob.size != 0) {
+    recordedVideo.controls = true;
+    recordedVideo.autoplay = true;
+    recordedVideo.src = url;
+    recordedVideo.id = "video";
+    recordedVideo.style.display = "block";
+    video_container.appendChild(recordedVideo);
+  }
+}
 
 // -----------------
